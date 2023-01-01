1a8d807a3c6ee189236b7d32bcd40d57233c4d0f
Ses Goe <jsesgoe@gmail.com>
2022-12-31 22:05:21 -0500

    added other symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,3 +1,17 @@
 # !/bin/sh
+
+# ln = link
+# -s = symbolic
+#
+# ln -s <source_file_or_directory> <result_filename>
+
 # nvim goes in ~/.config/nvim/
 cd ~/.config && ln -s ~/dotfiles/nvim nvim
+
+# zshrc, tmux, etc. go in ~
+cd ~ && ln -s ~/dotfiles/.p10k.zsh .p10k.zsh
+cd ~ && ln -s ~/dotfiles/.profile .profile 
+cd ~ && ln -s ~/dotfiles/.tmux.conf .tmux.conf
+cd ~ && ln -s ~/dotfiles/.zprofile .zprofile
+cd ~ && ln -s ~/dotfiles/.zprofile .zprofile
+cd ~ && ln -s ~/dotfiles/.zshrc .zshrc
