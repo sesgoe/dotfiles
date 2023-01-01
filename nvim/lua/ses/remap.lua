cdc54e8fa604103d016e8928aca257d65e70a89e
Ses Goe <jsesgoe@gmail.com>
2023-01-01 12:32:28 -0500

    Adding comments/refactoring

--- nvim/lua/ses/remap.lua
+++ nvim/lua/ses/remap.lua
@@ -1,41 +1,54 @@
+-- set leader key to space
 vim.g.mapleader = " "
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+local keymap = vim.keymap
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+------------------------
+-- General Keymaps
+------------------------
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Shift J in visual mode moves line up
+keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- Shift K in visual mode moves line down
+
+keymap.set("n", "J", "mzJ`z") -- When merging line up, keep cursor in same spot
+keymap.set("n", "<C-d>", "<C-d>zz") -- Keep cursor in middle when going page down
+keymap.set("n", "<C-u>", "<C-u>zz") -- Keep cursor in middle when going page up
+keymap.set("n", "n", "nzzzv") -- Keep cursor in middle when moving through results with 'n'
+keymap.set("n", "N", "Nzzzv") -- Keep cursor in middle when moving through results backwards with 'N'
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+keymap.set("x", "<leader>p", [["_dP]]) -- ??
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "Q", "<nop>")
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+keymap.set("n", "Q", "<nop>") -- Q is dumb, unbind it
+-- keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+keymap.set("n", "<leader>f", vim.lsp.buf.format) -- LSP formatter
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set('n', '<leader>e', [[:NvimTreeToggle<CR>]])
-vim.keymap.set('n', '<leader>t', [[:NvimTreeFocus<CR>]])
+keymap.set('n', '<leader>sv', '<C-w>v') -- split window vertically
+keymap.set('n', '<leader>sh', '<C-w>s') -- split window horizontally
+keymap.set('n', '<leader>se', '<C-w>=') -- make split windows equal width/height
+keymap.set('n', '<leader>sx', ':close<CR>') -- close current split window
 
-vim.keymap.set('n', '<leader>sv', '<C-w>v') -- split window vertically
-vim.keymap.set('n', '<leader>sh', '<C-w>s') -- split window horizontally
-vim.keymap.set('n', '<leader>se', '<C-w>=') -- make split windows equal width/height
-vim.keymap.set('n', '<leader>sx', ':close<CR>') -- close current split window
+------------------------
+-- Plugin Keymaps
+------------------------
 
+-- nvim-tree
+keymap.set('n', '<leader>e', [[:NvimTreeToggle<CR>]])
+keymap.set('n', '<leader>t', [[:NvimTreeFocus<CR>]])
+
+-- auto-format files on save
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format({ async = false })]]
--- nvim/lua/ses/set.lua
+++ nvim/lua/ses/set.lua
@@ -1,40 +1,47 @@
 local opt = vim.opt
 
+-- line numbers
 opt.nu = true
 opt.relativenumber = true
 
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
+-- tabs and indentation
+opt.tabstop = 4 -- spaces for tabs
+opt.softtabstop = 4 -- spaces for tabs
+opt.shiftwidth = 4 -- spaces for indent width
 opt.expandtab = true
-
 opt.smartindent = true
-opt.ignorecase = true
-opt.smartcase = true
 
+-- line wrapping
 opt.wrap = false
 
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true -- if you include mixed case in search, assumes case-sensitive
+
+-- cursor line
+opt.cursorline = true
+
+-- theme
+opt.termguicolors = true
+opt.signcolumn = "yes" -- always show sign column so editor doesn't shift
+opt.colorcolumn = "80" -- keep 80 col highlighted for reference for long text
+opt.scrolloff = 12 -- always keep at least X lines at bottom/top of buffer
+
+-- splitting should be more intuitive
+opt.splitright = true
+opt.splitbelow = true
+
+-- search
+opt.hlsearch = false
+opt.incsearch = true
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+--misc
+opt.isfname:append("@-@") -- files can have @ in filenames
+opt.updatetime = 50
 opt.swapfile = false
 opt.backup = false
 opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
-
-opt.hlsearch = false
-opt.incsearch = true
-
-opt.termguicolors = true
-
-opt.cursorline = true
-
-opt.scrolloff = 8
-opt.signcolumn = "yes"
-opt.isfname:append("@-@")
-
-opt.backspace = "indent,eol,start"
-
-opt.splitright = true
-opt.splitbelow = true
-
-opt.updatetime = 50
-
-opt.colorcolumn = "80"
